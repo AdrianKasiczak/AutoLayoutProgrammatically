d96d98036b6b0ce33cf3252d9e5e06f9af02c1de
Adrian Kasiczak <adrian.kasiczak@icloud.com>
2018-06-05 17:20:22 +0200

    80% of First View completed

--- AutoLayoutProgrammatically/ViewController.swift
+++ AutoLayoutProgrammatically/ViewController.swift
@@ -10,16 +10,65 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    let topImageViewContainer: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    let laptopImageView: UIImageView = {
+       let imageView = UIImageView(image: #imageLiteral(resourceName: "1laptop"))
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+    let descriptionTextView: UITextView = {
+       let textView = UITextView()
+        textView.text = "Are you ready to check out this beast?"
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        textView.font = UIFont.boldSystemFont(ofSize: 18)
+        textView.textAlignment = .center
+        textView.isEditable = false
+        textView.isScrollEnabled = false
+        return textView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = .white
+        
+        addingSubviews()
+        setupLayout()
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    private func addingSubviews(){
+        view.addSubview(descriptionTextView)
+        view.addSubview(topImageViewContainer)
+        
+        topImageViewContainer.addSubview(laptopImageView)
     }
-
+    private func setupLayout(){
+        
+        topImageViewContainer.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        topImageViewContainer.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        topImageViewContainer.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        topImageViewContainer.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.5).isActive = true
+        
+        laptopImageView.centerXAnchor.constraint(equalTo: topImageViewContainer.centerXAnchor).isActive = true
+        laptopImageView.centerYAnchor.constraint(equalTo: topImageViewContainer.centerYAnchor).isActive = true
+        laptopImageView.heightAnchor.constraint(equalTo: topImageViewContainer.heightAnchor, multiplier: 0.5).isActive = true
+        
+        descriptionTextView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        descriptionTextView.topAnchor.constraint(equalTo: topImageViewContainer.bottomAnchor).isActive = true
+        descriptionTextView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        descriptionTextView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        descriptionTextView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+    }
 
 }
 
